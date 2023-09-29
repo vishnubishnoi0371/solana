be91ae16504ad6af808339d1f417594474c93c28
Shubham-391 <138550036+Shubham-391@users.noreply.github.com>
2023-09-29 12:16:22 +0530

    Add files via upload

---
+++ assets/js/doc.js
@@ -0,0 +1,81 @@
+$('.slick-slider').slick({
+    infinite: true,
+    slidesToShow: 4,
+    centerMode: true,
+    variableWidth: true,
+    speed: 4000,
+    autoplay: true,
+    autoplaySpeed: 0,
+    cssEase: 'linear',
+    slidesToScroll: 1,
+    pauseOnHover: true,
+    responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1,
+            }
+        }
+    ]
+});
+
+AOS.init({
+    duration: 1000, // values from 0 to 3000, with step 50ms
+    easing: 'ease', // default easing for AOS animations
+    once: true,
+}
+);
+
+let a = document.getElementById("nav");
+let flow = document.querySelector(".flow");
+let cross4 = document.querySelector(".cross-4");
+let cross5 = document.querySelector(".cross-5");
+let cross6 = document.querySelector(".cross-6");
+let menuicon = document.querySelector("#menuIcon");
+let Home = document.querySelector(".Home");
+let Build = document.querySelector(".Build");
+let Network = document.querySelector(".Network");
+let Community = document.querySelector(".Community");
+
+
+
+menuicon.addEventListener("click", function () {
+    if (a.classList !== document.querySelector("fix-lft")) {
+        a.classList.toggle("fix-lft");
+        flow.classList.toggle("flow-hidden");
+        cross4.classList.toggle("cross-1");
+        cross5.classList.toggle("cross-2");
+        cross6.classList.toggle("cross-3")
+    }
+});
+
+Home.addEventListener("click", function () {
+    a.classList.toggle("fix-lft");
+    flow.classList.remove("flow-hidden");
+    cross4.classList.toggle("cross-1");
+    cross5.classList.toggle("cross-2");
+    cross6.classList.toggle("cross-3");
+});
+Build.addEventListener("click", function () {
+    a.classList.remove("fix-lft");
+    flow.classList.remove("flow-hidden");
+    cross4.classList.toggle("cross-1");
+    cross5.classList.toggle("cross-2");
+    cross6.classList.toggle("cross-3");
+});
+
+Network.addEventListener("click", function () {
+    a.classList.remove("fix-lft");
+    flow.classList.remove("flow-hidden");
+    cross4.classList.toggle("cross-1");
+    cross5.classList.toggle("cross-2");
+    cross6.classList.toggle("cross-3");
+});
+
+Community.addEventListener("click", function () {
+    a.classList.remove("fix-lft");
+    flow.classList.remove("flow-hidden");
+    cross4.classList.toggle("cross-1");
+    cross5.classList.toggle("cross-2");
+    cross6.classList.toggle("cross-3");
+});
